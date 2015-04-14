b5c11d1619d4f63caef8ed2312c6ee3d6da40e78
Levente Molnar <levente.molnar@kinja.com>
2015-04-14 15:30:00 +0200

    Separate one-transaction commands

--- src/main/scala/com/kinja/amqp/MessageStore.scala
+++ src/main/scala/com/kinja/amqp/MessageStore.scala
@@ -13,32 +13,6 @@ trait MessageStore {
 	 */
 	def deleteMessageUponConfirm(channelId: String, deliveryTag: Long): Int
 
-	/**
-	 * Within this method, the message store should make sure that
-	 * the loaded records are not updated/deleted by any other transaction.
-	 * If it can not be guaranteed AND multiple message repeater instances exist,
-	 * same repeaters may try to resend or delete the very same messages/confirmations at a given time
-	 */
-	def withLockingTransaction[T](f: => T): T
-
-	/**
-	 * Used in withLockingTransaction
-	 */
-	def loadMessageOlderThan(time: Long, exchangeName: String, limit: Int): List[Message]
-
-	/**
-	 * Used in withLockingTransaction
-	 */
-	def loadConfirmationByChannels(channelIds: List[String]): List[MessageConfirmation]
-
-	/**
-	 * Used in withLockingTransaction
-	 */
-	def deleteMessage(id: Long): Unit
-
-	/**
-	 * Used in withLockingTransaction
-	 */
-	def deleteConfirmation(id: Long): Unit
+	def createTransactionalStore: TransactionalMessageStore
 
 }
